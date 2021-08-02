9098fd51b41196b9d8fecce1eea59577d8e656f2
NewLanded <l1141041@163.com>
2021-08-02 14:27:30 +0800

    interface get_strategy_result_data sort result

--- source/note/note.py
+++ source/note/note.py
@@ -182,5 +182,6 @@ async def get_strategy_result_data(request: Request, request_params: GetStrategy
             "strategy_code": strategy_code,
             "data": strategy_data
         })
+    result.sort(key=lambda x: x["strategy_code"])
 
     return result
